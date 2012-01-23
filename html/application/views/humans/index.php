abca4668af4e3a9df417f633baecee89bc33639f
James Rowe <jim@jimcode.org>
2012-01-23 17:18:59 +0000

    Update html/application/views/humans/index.php

--- html/application/views/humans/index.php
+++ html/application/views/humans/index.php
@@ -1,13 +1,10 @@
-
 <div class="content">
 	<div class="page-header">
 		<h1><?php echo $human->name; ?> <small> the humans.txt</small></h1>
 	</div>
 	<div class="row">
 		<div class="span12">
-			<pre>
-			<?php echo htmlentities($human->txt); ?>
-			</pre>
+<pre><?php echo htmlentities($human->txt); ?></pre>
 			<p>Visit the <a href="http://<?php echo $human->name; ?>"><?php echo $human->name; ?></a> website.</p>
 		</div>
 		<div class="span2">
