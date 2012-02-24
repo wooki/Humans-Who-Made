99d3caacbb449bf64cbd60822372ed5e5db64bf6
= <=>
2012-02-24 13:16:08 +0000

    ignore does not exist text

--- hubot.rb
+++ hubot.rb
@@ -15,7 +15,7 @@ require 'mechanize'
 max_domains = 25
 
 # html markers
-html_markers = ['Page not found', '<META', '<script', '<SCRIPT', '<html>', '<HTML', '<!DOC', '<pre', '<html ', '<head ', '<body', '<p>', '<p ', '<a ', '<br>', '<br />']
+html_markers = ['does not exist', 'Page not found', '<META', '<script', '<SCRIPT', '<html>', '<HTML', '<!DOC', '<pre', '<html ', '<head ', '<body', '<p>', '<p ', '<a ', '<br>', '<br />']
 
 # connect
 db = Mysql.init
