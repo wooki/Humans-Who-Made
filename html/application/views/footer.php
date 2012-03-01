aba5569f30244cf94cd20c59ab929ff3599bb874
James Rowe <jim@jimcode.org>
2012-03-01 22:45:44 +0000

    testing disqus and share buttons

--- html/application/views/footer.php
+++ html/application/views/footer.php
@@ -10,13 +10,6 @@
   <script src="//ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
   <script>window.jQuery || document.write('<script src="js/libs/jquery-1.6.2.min.js"><\/script>')</script>
   
-  <script>
-    window._gaq = [['_setAccount','UA-29642156-1'],['_trackPageview'],['_trackPageLoadTime']];
-    Modernizr.load({
-      load: ('https:' == location.protocol ? '//ssl' : '//www') + '.google-analytics.com/ga.js'
-    });
-  </script>
-  
   <!-- Prompt IE 6 & 7 users to install Chrome Frame. Remove this if you want to support IE 6.
        chromium.org/developers/how-tos/chrome-frame-getting-started -->
   <!--[if lt IE 8 ]>
