bcb624ead0dc0d752f4df330f1f7a875d2180eea
James Rowe <jim@jimcode.org>
2012-03-01 21:52:30 +0000

    google tracking

--- html/application/views/footer.php
+++ html/application/views/footer.php
@@ -10,16 +10,13 @@
   <script src="//ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
   <script>window.jQuery || document.write('<script src="js/libs/jquery-1.6.2.min.js"><\/script>')</script>
   
-  <!-- Change UA-XXXXX-X to be your site's ID -->
-  <!--
   <script>
-    window._gaq = [['_setAccount','UAXXXXXXXX1'],['_trackPageview'],['_trackPageLoadTime']];
+    window._gaq = [['_setAccount','UA-29642156-1'],['_trackPageview'],['_trackPageLoadTime']];
     Modernizr.load({
       load: ('https:' == location.protocol ? '//ssl' : '//www') + '.google-analytics.com/ga.js'
     });
   </script>
-  -->
-
+  
   <!-- Prompt IE 6 & 7 users to install Chrome Frame. Remove this if you want to support IE 6.
        chromium.org/developers/how-tos/chrome-frame-getting-started -->
   <!--[if lt IE 8 ]>
