da1e54b220b805298478143f48a0e5da8297aa67
James Rowe <jim@jimcode.org>
2012-03-01 23:15:33 +0000

    testing disqus and share buttons

--- html/application/views/humans/index.php
+++ html/application/views/humans/index.php
@@ -4,11 +4,6 @@
 	</div>
 	<div class="row">
 		<div class="span9">
-<a href="https://twitter.com/share" class="twitter-share-button" data-via="HumansWhoMade">Tweet</a>
-<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
-
-<div class="fb-like" data-href="http://humanswhomade.com" data-send="false" data-layout="button_count" data-show-faces="false"></div>
-
 
 			<?php if ((!is_null($human->title)) and ($human->title != '')) { ?>
 			<h2><?php echo $human->title; ?></h2>
@@ -16,6 +11,17 @@
 			<?php if ((!is_null($human->description)) and ($human->description != '')) { ?>
 			<p><?php echo $human->description; ?></p>
 			<?php } ?>
+
+
+<p>
+<a href="https://twitter.com/share" class="twitter-share-button" data-via="HumansWhoMade">Tweet</a>
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+
+<g:plusone size="small" annotation="inline"></g:plusone>
+
+<div class="fb-like" data-href="http://humanswhomade.com" data-send="false" data-layout="button_count" data-show-faces="false"></div>
+</p>
+
 <pre>
 <?php echo $human->txt; ?>
 </pre>
